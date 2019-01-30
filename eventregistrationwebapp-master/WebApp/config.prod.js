8b97474fb353e3aadc46b9d2541241ce61ce079f
rathi-payal1992 <47178104+rathi-payal1992@users.noreply.github.com>
2019-01-30 21:06:47 +0530

    Update config.prod.js

--- eventregistrationwebapp-master/WebApp/config.prod.js
+++ eventregistrationwebapp-master/WebApp/config.prod.js
@@ -16,7 +16,7 @@ exports.creds = {
   responseMode: 'form_post', 
 
   // Required, the reply URL registered in AAD for your app
-  redirectUrl: 'https://anildwaeventregistration.azurewebsites.net/auth/openid/return', 
+  redirectUrl: 'https://payals-webapp.azurewebsites.net/auth/openid/return', 
 
   // Required if we use http for redirectUrl
   allowHttpForRedirectUrl: true,
