c690067baf4879380004ff7a8b2085e5685cdc96
rathi-payal1992 <47178104+rathi-payal1992@users.noreply.github.com>
2023-11-10 18:43:43 +0530

    Update test_array.py

--- test_array.py
+++ test_array.py
@@ -1,20 +1,27 @@
 import sys
 environments= sys.argv[1]
 list_of_environments=environments.split(",")
-for position, name in enumerate(list_of_environments):
-    print(f"{name}: {position}")
-    match name:
-        case 'S1':
+def modify_array:
+   for position, name in enumerate(list_of_environments):
+       print(f"{name}: {position}")
+       match name:
+         case 'S1':
             print('this is S1')
             list_of_environments[position]='S1+whteverstrng'
-        case 'S2':
+         case 'S2':
             print('this is S2')
             list_of_environments[position]='S2+whteverstrng'
  
         # case 418:
             # return "I'm a teapot"
 
-        # If an exact match is not confirmed, this last case will be
+        # If an exact match is not confirmed, this last case will be used if provided
         case _:
             print("Wrong env name")
-#return list_of_environments
+    return list_of_environments
+
+def main():
+   modify_array()
+
+if __name__ == "__main__":
+    main()
