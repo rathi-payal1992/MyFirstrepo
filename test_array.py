7ed3535dfa2e5b22d720f9d022070e5dea26eb85
rathi-payal1992 <47178104+rathi-payal1992@users.noreply.github.com>
2023-11-13 17:40:20 +0530

    Update test_array.py

--- test_array.py
+++ test_array.py
@@ -28,7 +28,7 @@ def modify_array():
 # if __name__ == "__main__":
     # main()
 ret=modify_array()
-os.system(f'echo "{OUTPUT_LIST}={ret}" >> $GITHUB_OUTPUT')
+os.system(f'echo "OUTPUT_LIST=ret" >> $GITHUB_OUTPUT')
 # with open(os.environ['GITHUB_OUTPUT'],'a') as fh:
 #    print('OUTPUT_LIST={ret}',file=fh)
 #>> ${GITHUB_OUTPUT}
